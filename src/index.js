6cba55930e48fc05e1d739126c11afbd3ea6604a
aryan21710 <aryan21710@gmail.com>
2021-03-23 13:23:17 +0530

    ADDED MANUAL SCRIPT IN PACKAGE.JSON TO RUN TYPECHECK USING TYPESCRIPT

---
+++ src/index.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.__esModule = true;
+var react_1 = require("react");
+var react_dom_1 = require("react-dom");
+var App = function () { return <h1> START APP USING TS </h1>; };
+react_dom_1["default"].render(<App />, document.getElementById('app'));
